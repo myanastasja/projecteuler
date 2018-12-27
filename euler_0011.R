bc70c137c909cd4de2d3db6a1828619faf1d2927
Antonela Mamuca <antonela.mamuca@m2bit.com>
2018-12-27 20:38:43 +0100

    Problem 11 and environment

--- euler_0011.R
+++ euler_0011.R
@@ -48,13 +48,13 @@ x <- "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
-
-grep(x, "^\\")
-
-grep("ac*b", strings, value = TRUE)
-
 x <- strsplit(x, " ")
 x <- unlist(x)
-x <- as.numeric(x)
+length(x)
 
+for(i in seq(20, length(x), 19))
+    x[i] <- substr(x[i], 1, 2)
+    
+x <- as.numeric(x)
+x <- matrix(x, nrow = 20)
 
